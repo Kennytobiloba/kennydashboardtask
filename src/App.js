0cca2b3430e4b69ea0e108a2da01c1a6a55e4b63
kennytobiloba <olaatunbikehinde@gmail.com>
2023-09-11 09:58:47 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
+import { SideBar } from "./components/SideBar";
+import { Main } from "./components/Main";
+import { Activity } from "./components/Activity";
+
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className=" flex justify-center w-full items-center bg-[#0a0325] h-full ">
+        <div className="flex justify-between w-[100%] px-5 py-4">
+        
+<SideBar/>
+<Main/>
+<Activity/>
+
+
+        </div>
+
+      </div>
+     
     </div>
   );
 }
